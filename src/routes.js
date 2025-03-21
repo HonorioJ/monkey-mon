8367bececff9531e814e0e3b749f01f483092722
lucasIbanhez <lucasibanhezalmeida@gmail.com>
2025-03-21 12:57:50 -0300

    Change BrowserRouter to HashRouter

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import PokemonInfo from './pages/PokemonInfo'
 import Header from './components/Header'
@@ -6,14 +6,14 @@ import Team from './pages/Team'
 
 function RoutesApp(){
   return(
-    <BrowserRouter>
+    <HashRouter>
     <Header/>
       <Routes>
         <Route path='/' element={ <Home/> }/>
         <Route path='pokemon/:name' element={ <PokemonInfo/> }/>
         <Route path='/team' element={ <Team/> } />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
